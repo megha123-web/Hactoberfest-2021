2ae395ea57e6d7fc546a5f2371a32fcf7d0c584c
Meghasinha234 <87225255+Meghasinha234@users.noreply.github.com>
2021-10-23 09:58:03 +0530

    lower case to uppercase letter of string using iteration

---
+++ lower to upper.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    for(int i=0;i<str.length();i++){
+        if(str[i]>='a'&&str[i]<='z'){
+        str[i]=str[i]-32;
+        }
+
+    }
+    cout<<str<<endl;
+    return 0;
+}
